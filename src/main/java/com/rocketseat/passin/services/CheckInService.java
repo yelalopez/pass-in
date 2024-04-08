5ed9982a84c6f4ef20ba0b700381bd4a392bc55a
yelalopez <yelitzalopezduque@gmail.com>
2024-04-08 14:32:34 -0300

    feat: services

---
+++ src/main/java/com/rocketseat/passin/services/CheckInService.java
@@ -0,0 +1,37 @@
+package com.rocketseat.passin.services;
+
+import com.rocketseat.passin.domain.attendee.Attendee;
+import com.rocketseat.passin.domain.checkin.CheckIn;
+import com.rocketseat.passin.domain.checkin.exceptions.CheckInAlredyExistsException;
+import com.rocketseat.passin.repositories.CheckInRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CheckInService {
+
+    private final CheckInRepository checkInRepository;
+
+    public void registerCheckIn(Attendee attendee){
+        this.verifyCheckInExists(attendee.getId());
+
+        CheckIn newCheckIn = new CheckIn();
+        newCheckIn.setAttendee(attendee);
+        newCheckIn.setCreatedAt(LocalDateTime.now());
+
+        this.checkInRepository.save(newCheckIn);
+    }
+
+    private void verifyCheckInExists(String attendeeId){
+        Optional<CheckIn> isCheckedIn = this.getCheckIn(attendeeId);
+        if(isCheckedIn.isPresent()) throw new CheckInAlredyExistsException("Attendee alredy checked in");
+    }
+    public Optional<CheckIn> getCheckIn(String attendeeId){
+        return this.checkInRepository.findByAttendeeId(attendeeId);
+
+    }
+}
